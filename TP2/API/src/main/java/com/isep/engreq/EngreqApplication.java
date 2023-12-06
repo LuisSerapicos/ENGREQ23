c75f6498cf96aada77140f8d8fa7075e68333aa4
Luís Peixoto <luis.peixoto@ceiia.com>
2023-12-06 19:51:34 +0000

    Api Init

---
+++ TP2/API/src/main/java/com/isep/engreq/EngreqApplication.java
@@ -0,0 +1,13 @@
+package com.isep.engreq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngreqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngreqApplication.class, args);
+	}
+
+}
