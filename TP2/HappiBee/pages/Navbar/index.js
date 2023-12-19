853effb52c47d1bfe5264e4e9b945283ed2616c1
Luís Peixoto <1230198@isep.ipp.pt>
2023-12-19 23:39:36 +0000

    Feature: Initial Version, Change the Technology used to React Native

---
+++ TP2/HappiBee/pages/Navbar/index.js
@@ -0,0 +1,15 @@
+// components/Navbar/index.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Text>NavBar</Text>
+        <Text>Some text</Text>
+        <Text>Some text</Text>
+        <Text>Some text</Text>
+    </nav>
+  );
+};
+
+export default Navbar;
