50b870990ad86de2ce2b20604a29d176ad51f356
Ana Matias <1210081@isep.ipp.pt>
2024-01-07 02:12:08 +0000

    ENGREQ-31 Added firebase implementation in Inspecao

--- TP2/AndroidApp/HappiBee/app/src/main/java/com/example/happibee/Presentation/Inspecao/ViewModel/AddInspecaoViewModel.kt
+++ TP2/AndroidApp/HappiBee/app/src/main/java/com/example/happibee/Presentation/Inspecao/ViewModel/AddInspecaoViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.happibee.Presentation.Inspecao.ViewModel
 
+import android.util.Log
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,9 +13,12 @@ import com.example.happibee.Data.Model.Apiario
 import com.example.happibee.Data.Model.Inspecao
 import com.example.happibee.Data.UseCases.Apiario.ApiarioUseCase
 import com.example.happibee.Data.UseCases.Inspecao.InspecaoUseCase
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -49,6 +53,25 @@ class AddInspecaoViewModel @Inject constructor(
     }
 
     fun addInspecao()=viewModelScope.launch {
-        useCase.insertInspecao(Inspecao(date, observations, apiarioId = id!!))
+        //useCase.insertInspecao(Inspecao(date, observations, apiarioId = id!!))
+        try {
+            val novaInspecao = Inspecao(
+                date = date,
+                observations = observations,
+                apiarioId = 1,
+                apicultorId = 1
+            )
+
+            Firebase.firestore.collection("apicultores")
+                .document("apicultor1")
+                .collection("apiarios")
+                .document("apiario1")
+                .collection("inspecao")
+                .document("inspecao2")
+                .set(novaInspecao)
+                .await()
+        } catch (e: Exception) {
+            Log.e("ADD_INSPECAO", "Erro ao adicionar inspeção: ${e.message}")
+        }
     }
 }
