23f1ca471ada73875fc9a8118b0b963af3b20fff
Ana Matias <1210081@isep.ipp.pt>
2024-01-07 23:52:50 +0000

    Updated Documentation, added offline feature and wip notifications feature

---
+++ TP2/AndroidApp/HappiBee/app/src/main/java/com/example/happibee/APIs/MyFirebaseMessagingService.kt
@@ -0,0 +1,26 @@
+package com.example.happibee.APIs
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "FCMService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+    }
+}
--- TP2/AndroidApp/HappiBee/app/src/main/java/com/example/happibee/HappiBeeApp.kt
+++ TP2/AndroidApp/HappiBee/app/src/main/java/com/example/happibee/HappiBeeApp.kt
@@ -2,6 +2,8 @@ package com.example.happibee
 
 import android.app.Application
 import com.example.happibee.Data.Database.DatabaseBootstrapper
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -18,5 +20,11 @@ class HappiBeeApp:Application() {
         CoroutineScope(Dispatchers.IO).launch {
             databaseBootstrapper.initializeData()
         }
+
+        val firestoreSettings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+
+        FirebaseFirestore.getInstance().firestoreSettings = firestoreSettings
     }
 }
